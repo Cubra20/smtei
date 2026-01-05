6d68be5680dee78cdeecbf893d49a494c39bcc91
Ahron khyle Mediavillo <ahronkhylemediavillo@gmail.com>
2026-01-05 08:42:14 +0000

    System generated commit, initial commit

---
+++ pages/Dashboard/jsobjects/JSObject2/JSObject2.js
@@ -0,0 +1,16 @@
+export default {
+getDashboardMetrics:async () => {
+	const Perlevel =await getPerlevel.run();
+	
+	return{
+		sumtotal:Perlevel.length,
+		grade7:Perlevel.filter(e => e.PROVINCE === "G7" ).length,
+		grade8:Perlevel.filter(e => e.PROVINCE === "G8" ).length,
+		grade9:Perlevel.filter(e => e.PROVINCE === "G9" ).length,
+		grade10:Perlevel.filter(e => e.PROVINCE === "G10" ).length,
+		grade11:Perlevel.filter(e => e.PROVINCE === "G11" ).length,
+		grade12:Perlevel.filter(e => e.PROVINCE === "G12" ).length
+	}
+}
+
+}
