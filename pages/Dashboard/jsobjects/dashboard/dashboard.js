509255a8e7e8e1a7b7a94d6c31f9191ece1b8b79
Ahron khyle Mediavillo <ahronkhylemediavillo@gmail.com>
2026-01-06 09:07:56 +0000

    1 Track Applications removed 2 Application Upload removed ASSESSMENT CENTER added Dashboard modified SMTEI removed Student List modified TRAINING_CENTER added 2 datasource removed Application settings modified 2 module instances modified

--- pages/Dashboard/jsobjects/dashboard/dashboard.js
+++ pages/Dashboard/jsobjects/dashboard/dashboard.js
@@ -1,19 +1,20 @@
 export default {
 getDashboardMetrics:async () => {
 	const Perlevel =await getPerlevel.run();
-	const Allqualification =await getAllqualification.run();
+	const Assessment =await getAssessment.run();
+	const Training =await getTraining.run();
 	
 	
 	return{
 		sumtotal:Perlevel.length,
-		grade7:Perlevel.filter(e => e.PROVINCE === "G7" ).length,
-		grade8:Perlevel.filter(e => e.PROVINCE === "G8" ).length,
-		grade9:Perlevel.filter(e => e.PROVINCE === "G9" ).length,
-		grade10:Perlevel.filter(e => e.PROVINCE === "G10" ).length,
-		grade11:Perlevel.filter(e => e.PROVINCE === "G11" ).length,
-		grade12:Perlevel.filter(e => e.PROVINCE === "G12" ).length,
-		training:Allqualification.filter(a => a.TYPE === "TRAINING CENTER").length,
-		assessment:Allqualification.filter(a => a.TYPE === "ASSESSMENT CENTER").length
+		grade7:Perlevel.filter(c => c.GRADE_LEVEL === "G7" ).length,
+		grade8:Perlevel.filter(c => c.GRADE_LEVEL === "G8" ).length,
+		grade9:Perlevel.filter(c => c.GRADE_LEVEL === "G9" ).length,
+		grade10:Perlevel.filter(c => c.GRADE_LEVEL === "G10" ).length,
+		grade11:Perlevel.filter(c => c.GRADE_LEVEL === "G11" ).length,
+		grade12:Perlevel.filter(c => c.GRADE_LEVEL === "G12" ).length,
+		training:Training.filter(a => a.TYPE === "TRAINING CENTER").length,
+		assessment:Assessment.filter(a => a.TYPE === "ASSESSMENT CENTER").length
 	} 
 }
 
