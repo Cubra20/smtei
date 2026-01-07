b8e634ed7e1c301034d9b6eebcfec299ff02775f
Ahron khyle Mediavillo <ahronkhylemediavillo@gmail.com>
2026-01-07 08:13:20 +0000

    ASSESSMENT CENTER modified 1 query modified 2 jsObjects modified applicant added competent modified

--- pages/ASSESSMENT CENTER/jsobjects/competent/competent.js
+++ pages/ASSESSMENT CENTER/jsobjects/competent/competent.js
@@ -3,11 +3,24 @@ export default {
 	countFromOtherTable(data_table, Allapplicants) {
     if (!data_table) return 0;
 
-    return Allapplicants.filter(row =>
+    return {
+		competent:Allapplicants.filter(row =>
       row.QUALIFICATION === data_table.Q_CODE &&
       row.STATUS === "Competent"
-    ).length;
+    ).length,
+		applicant:Allapplicants.filter(row =>
+      row.QUALIFICATION === data_table.Q_CODE &&
+      row.STATUS === "Applicant"
+    ).length,
+		schedule:Allapplicants.filter(row =>
+      row.QUALIFICATION === data_table.Q_CODE &&
+      row.STATUS === "Paid"
+    ).length,
+		assessment:Allapplicants.filter(row =>
+      row.QUALIFICATION === data_table.Q_CODE &&
+      row.STATUS === "For schedule"
+    ).length
   }
+	}
+ 	} 
 
-	} 
-;
